2ede6816dd017efcafa981b8b2f95841c28a7b7c
HuKraft <56226696+HuKraft@users.noreply.github.com>
2019-10-07 01:14:40 +0100

    scaffolding + unfinished test
    I added comments which represent - on a very basic level - what the code might look like overall. Then, I gave it a corresponding test file, using unittest. The only thing the file tests is the function is_empty - which I assume I'll be using at some point.

---
+++ src/main.py
@@ -0,0 +1,7 @@
+#add data structure
+
+#add is_empty
+
+#add search
+
+#add blah blah
---
+++ src/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestMain(unittest.TestCase):
+
+    def test_is_empty(self):
+        #create empty node named N
+        #N = blah blah
+        result = is_empty(N)
+        self.assertTrue(result)
+
+if "__name__" == "__main__";
+    unittest.main()
