22b7735cc2dd73067a6b98127d514169058c459b
HuKraft <56226696+HuKraft@users.noreply.github.com>
2019-10-07 23:19:26 +0100

    added scaffolding and created real tests
    The "scaffolding" refers to the data structure - as I thought I should at least define that to have a clear way of writing my tests. Then , I created three tests but only wrote one function.
    
    After I have a better idea as to how robust my program is meant to be (ie should I make sure that the nodes of which we need to find the LCA exist within the binary tree, among other things) I will complete the tests and write the corresponding functions

--- src/main.py
+++ src/main.py
@@ -5,3 +5,20 @@
 #add search
 
 #add blah blah
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class bintree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def is_empty(self):
+        return (self.root.left == None and self.root.right == None)
+
+tree = bintree(1)
+tree.root.left = Node(1)
+tree.root.left = Node(1)
--- src/test_main.py
+++ src/test_main.py
@@ -1,12 +1,34 @@
 import unittest
+from main import Node
+from main import bintree
 
 class TestMain(unittest.TestCase):
 
     def test_is_empty(self):
-        #create empty node named N
-        #N = blah blah
-        result = is_empty(N)
-        self.assertTrue(result)
+        #testing for a binary tree which only has a root -
+        #no point testing for bintree with empty root: it would be the same
+        t = bintree(1)
+        self.assertTrue(bintree.is_empty(t))
+        #testing for a binary tree which is non-empty
+        t.root.left = 3
+        self.assertFalse(bintree.is_empty(t))
 
-if "__name__" == "__main__";
-    unittest.main()
+    def test_exists(self): #testing for a function which makes sure a node exists
+        #testing for trying to find a value in a binary which is totally is_empty
+        #(not sure if I should add this node - depends on robustness wanted)
+        b = bintree(None)
+        value = 1
+        exists(b, value)
+        #if
+    def test_lca(self):
+        #testing for a tree which has takes the same two values (call them p and q)
+        p = 1
+        q = 1
+        t = bintree(1)
+        t.node.left = 2
+        t.node.right = 3
+        self.assertEqual(bintree.lca(t, p, q), 1)
+        #more testing to add based on the robustness I'm looking for (will ask teacher)
+
+#if "__name__" == "__main__":
+#    unittest.main()
