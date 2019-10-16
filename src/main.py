da92a5a92ad48aa512fb083b7edbdf1e48390034
HuKraft <56226696+HuKraft@users.noreply.github.com>
2019-10-16 17:07:12 +0100

    Finished the programme
    As described in the last commit, I realized that most of my tests were necessary - I overcomplicated the problem. Thus,  I took out the functions which helped identify if a tree was valid (properly defined), and added some extra tests to the LCA function.
    
    The result is only one test and one corresponding method, which, using the unit testing, works as I wanted it to.

--- src/main.py
+++ src/main.py
@@ -13,23 +13,8 @@ class Node(object):
         self.left = None
         self.right = None
 
-    def is_empty(self):
-        return (self.value == None)
-    def is_empty_root(self):
-        return (self.left == None) and (self.right == None)
-
-    def tree_valid_list(self):
-        if self == None:
-            return []
-        return [self.value] + self.left.tree_valid_list() + self.right.tree_valid_list()
-
-    def tree_valid(self, p, q):
-        if ((not p in tree_valid_list) or (not q in tree_valid_list)):
-            return False
-        return True
-
     def lca(self, p, q):
-        if self is None:
+        if self == None:
             return None
         left_lca = None
         right_lca = None
@@ -43,13 +28,8 @@ class Node(object):
             return self.value
         return left_lca if left_lca else right_lca
 
-    def lca_t(self,p,q):
-        if not self.tree_valid(p,q):
-            return None
-        return self.lca(p,q)
-        return 1;
-
-t = Node(2)
-t.left = Node(3)
-t.right = Node(4)
-print(t.lca(5,6))
+b_1 = Node(1)
+p = 1
+q = 2
+result = Node.lca(b_1, p, q)
+print(result)
--- src/test_main.py
+++ src/test_main.py
@@ -4,81 +4,45 @@ from main import Node
 
 class TestMain(unittest.TestCase):
 
-    def test_is_empty(self):
-        #testing for a binary tree which only has a root -
-        #no point testing for Node with empty root: it would be the same
-        t = Node(1)
-        self.assertTrue(Node.is_empty(t))
-        #testing for a binary tree which is non-empty
-        t.left = 3
-        self.assertFalse(Node.is_empty(t))
-
-    def test_tree_valid(self):#function which tests if a tree is valid (makes it robust)
-        #empty tree
-        b = Node(None)
-        p = 3
-        q = 5
-        result = Node.tree_valid(b, p, q, [])
-        self.assertTrue(result)
-        #tree which is valid and holds the values asked for
-        b = Node(1)
-        p = 3
-        q = 5
-        b.left = Node(5)
-        b.right = Node(3)
-        result = Node.tree_valid(b, p, q, [])
-        self.assertTrue(result)
-        #tree which is valid but does NOT hold the values asked for
-        p = 12
-        q = 5
-        result = Node.tree_valid(b, p, q, [])
-        self.assertFalse(result)
-        #invalid tree (has a value appear more than once)
-        b.left.left = Node(5)
-        result = Node.tree_valid(b, p, q, [])
-        self.assertFalse(result)
-        #invalid tree (there exists a node that is not of type Node)
-        b.left.right = 3
-        result = Node.tree_valid(b, p, q, [])
-        self.assertFalse(result)
-        return
-
     def test_lca(self):
         #test 1 - empty tree
         b_1 = Node(None)
         p = 1
         q = 2
         result = Node.lca(b_1, p, q)
-        self.assertEqual(b_1, -1)
+        self.assertEqual(result, None)
         #test 2 - a tree which only has a root
         b_1 = Node(1)
         p = 1
         q = 2
         result = Node.lca(b_1, p, q)
-        self.assertEqual(result, -1)
+        self.assertEqual(result, 1)
         #test 3 -  tree which is missing a value that is asked for
         p = 4
         b_1.left = Node(3)
         b_1.right = Node(2)
-        result = Node.lca(b_1)
-        self.assertEqual(result, -1)
+        result = Node.lca(b_1, p, q)
+        self.assertEqual(result, 2) #if p found and q not (or vice-versa), it will return that value
         #test 4 - tree in which p is the root
-        p=1
-        result = Node.lca(b_1)
-        self.assertEqual(result, -1)
-        #test 5 -  tree where p = q
         p = 1
-        q = 1
-        t = Node(1)
-        t.left = Node(2)
-        t.right = Node(3)
-        self.assertEqual(Node.lca(t, p, q), 1)
-        #testing for a tree in which p is a parent of q
-        p = 5
         q = 2
-        b_1.right.right = Node(5)
-        result = Node.lca(b_1)
+        result = Node.lca(b_1, 1, 2)
         self.assertEqual(result, 1)
+        #test 5 -  tree where p = q
+        p = 2
+        result = Node.lca(b_1, p, q)
+        self.assertEqual(result, 2)
+        #test 6 - where p is a parent of q
+        p = 2
+        q = 5
+        b_1.right.right = Node(5)
+        result = Node.lca(b_1, p ,q)
+        self.assertEqual(result, 2)
+        #tree which is valid but does NOT hold the values asked for
+        p = 12
+        q = 19
+        result = Node.lca(b_1, p,q)
+        self.assertEqual(result, None)
 
 
 
